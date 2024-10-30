920796c418e8559df07f488d83482b6c64a78e78
Milad Raeisi <miladsoft@yahoo.com>
2024-10-30 22:13:04 +0400

    Update AngorStepsAnimation.jsx

--- src/components/AngorStepsAnimation.jsx
+++ src/components/AngorStepsAnimation.jsx
@@ -14,8 +14,9 @@ const AngorStepsAnimation = () => {
   const stepRefs = useRef([]);
 
   useEffect(() => {
-    if (currentStep > 0) {
-      gsap.fromTo(stepRefs.current[currentStep], { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 1.2, ease: "power4.out" });
+    if (currentStep >= 0) {
+      gsap.fromTo(stepRefs.current[currentStep], { opacity: 0, x: -50, scale: 0.8 }, 
+      { opacity: 1, x: 0, scale: 1, duration: 1, ease: "power4.out" });
     }
   }, [currentStep]);
 
@@ -26,20 +27,24 @@ const AngorStepsAnimation = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center -mx-10 text-white p-8 mb-24">
-      {steps.map((step, index) => (
-        <p
-          key={index}
-          ref={(el) => (stepRefs.current[index] = el)}
-          className={`text-lg max-w-3xl text-center my-4 ${index > currentStep ? 'hidden' : ''}`}
-        >
-          {step}
-        </p>
-      ))}
+    <div className="flex flex-col items-center justify-center mb-24 text-white px-4 py-8">
+      <div className="w-full sm:w-10/12 md:w-8/12 lg:w-7/12">
+        {steps.map((step, index) => (
+          <p
+            key={index}
+            ref={(el) => (stepRefs.current[index] = el)}
+            className={`text-base sm:text-lg md:text-xl lg:text-2xl text-center transition-all duration-700 transform ${
+              index === currentStep ? 'block' : 'hidden'
+            } my-6`}
+          >
+            {step}
+          </p>
+        ))}
+      </div>
       {currentStep < steps.length - 1 && (
         <button
           onClick={handleNextStep}
-          className="mt-6 px-6 py-2 bg-blue-500 text-lg rounded-lg hover:bg-blue-700 transition duration-300 ease-in-out"
+          className="mt-8 px-6 py-2 bg-blue-500 text-base md:text-lg rounded-full hover:bg-blue-600 hover:scale-105 transform transition duration-300 ease-out shadow-lg"
         >
           Load Next Step
         </button>
