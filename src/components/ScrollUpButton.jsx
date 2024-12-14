3751a1ca590f7e7c08b8b8ad6b3ce4c9350106aa
Milad Raeisi <miladsoft@yahoo.com>
2024-12-14 20:31:58 +0400

    Add new content and settings files, update dependencies, and enhance responsiveness

--- src/components/ScrollUpButton.jsx
+++ src/components/ScrollUpButton.jsx
@@ -5,6 +5,9 @@ export const ScrollUpButton = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", toggleVisible);
+    return () => {
+      window.removeEventListener("scroll", toggleVisible);
+    };
   }, []);
 
   const toggleVisible = () => {
@@ -19,35 +22,35 @@ export const ScrollUpButton = () => {
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: "smooth", // Ensure smooth scrolling
     });
   };
 
   return (
     <>
-      {isVisible && (
-        <button
-          className="w-12 h-12 fixed bottom-6 right-6 main-border-gray rounded-xl  bg-bgDark2 hover:bg-bgDark3 cursor-pointer flex justify-center items-center transition z-50"
-          onClick={scrollToTop}
-          aria-label="Scroll to top"
+      <button
+        className={`w-12 h-12 fixed bottom-6 right-6 main-border-gray rounded-xl bg-bgDark2 hover:bg-bgDark3 cursor-pointer flex justify-center items-center transition-opacity duration-300 ease-in-out z-50 ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
+        onClick={scrollToTop}
+        aria-label="Scroll to top"
+      >
+        <svg
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width="35px"
+          height="35px"
+          viewBox="0 0 20 20"
         >
-          <svg
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            width="35px"
-            height="35px"
-            viewBox="0 0 20 20"
-          >
-            <path
-              d="M4.16732 12.5L10.0007 6.66667L15.834 12.5"
-              stroke="rgb(203, 221, 225)"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></path>
-          </svg>
-        </button>
-      )}
+          <path
+            d="M4.16732 12.5L10.0007 6.66667L15.834 12.5"
+            stroke="rgb(203, 221, 225)"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></path>
+        </svg>
+      </button>
     </>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,7 +23,7 @@ module.exports = {
         Inter: "Inter",
       },
       screens: {
-        xs: "530px",
+        xs: "480px", 
         sm: "640px",
         md: "768px",
         lg: "1024px",
